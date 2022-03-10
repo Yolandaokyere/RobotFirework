bfe68f334195dc4f6ef01df7bf96843c493f4c2a
Yolandaokyere <yolandaokyere@outlook.com>
2022-03-10 10:35:47 +0100

    Co-authored-by: Daan Korver <DaanKorver@users.noreply.github.com>

--- swan/script.js
+++ swan/script.js
@@ -1,13 +1,9 @@
 var body = document.querySelector('body')
-console.log(body)
 
-<<<<<<< HEAD
-=======
 body.addEventListener('click', function() {
     body.classList.toggle('darkmode')
 })
 
->>>>>>> 6f68ef14441824e16f4b012c3ba81929fec8430d
 var buttonPlay = document.querySelector('#music')
 console.log(buttonPlay)
 var musiclist = document.querySelector('audio')
@@ -17,28 +13,13 @@ console.log(musiclist)
 // functie buttonplay tekst = gekoppeld. en andersom
 
 buttonPlay.addEventListener('click', function() {
-if(musiclist.paused){
-    musiclist.play()
-    buttonPlay.querySelector('img').src ="images/pausebutton.png";
-<<<<<<< HEAD
-} else {
-    musiclist.pause()
-    buttonPlay.querySelector('img').src ="images/play.png";
-
-=======
-    body.classList.toggle('darkmode')
-} else {
-    musiclist.pause()
-    buttonPlay.querySelector('img').src ="images/play.png";
-    body.classList.toggle('darkmode')
->>>>>>> 6f68ef14441824e16f4b012c3ba81929fec8430d
-}
+    if(musiclist.paused){
+        musiclist.play()
+        buttonPlay.querySelector('img').src ="images/pausebutton.png";
+        
+    } else {
+        musiclist.pause()
+        buttonPlay.querySelector('img').src ="images/play.png";
+        body.classList.toggle('darkmode')
+    }
 })
-
-
-<<<<<<< HEAD
-body.addEventListener('click', function() {
-    body.classList.toggle('darkmode')
-})
-=======
->>>>>>> 6f68ef14441824e16f4b012c3ba81929fec8430d
